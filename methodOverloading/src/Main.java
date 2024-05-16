dea0b0c90d783ab186db5e16ebeeb428525b8844
umtlh <umtlh@hotmail.com>
2024-05-16 13:44:50 +0300

    methodoverloading dersi alındı.

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        dortIslem DortIslem = new dortIslem();
+        System.out.println(DortIslem.topla(2,3));
+        System.out.println(DortIslem.topla(2,3,4));
+    }
+}
