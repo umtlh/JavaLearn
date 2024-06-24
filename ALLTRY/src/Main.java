723506da6d1854f8c00146efc71cbeb1576788fd
umtlh <umtlh@hotmail.com>
2024-06-24 12:40:13 +0300

    ALLTRY HATIRLATICI DENEMELER YAPILDI. Matematiksel Operatörler - Artırma ve Eksiltme İşlemleri , Scanner - Konsoldan Girdi Alma, Koşullu İşlemler, Switch Case, Döngüler (While, For, vb. gibi işlemler gözden geçirildi.)

---
+++ ALLTRY/src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       /* int a = 32;
+        double b = 3.14;
+        float c = 3.14f;
+        char d = 'A';
+        String test = "Test";
+        boolean e = false;
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+        System.out.println("c:"+c);
+        System.out.println("d:"+d);
+        System.out.println("Test:"+ test);
+        System.out.println("e:"+false);
+        */
+        /* int a = 20;
+        int b = 15;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/(double)b);
+        System.out.println(a%b); // ( % işareti bölümünden kalanı ifade eder)
+         */
+        //ARTTIRMA
+//        int a = 5;
+//        a = a+1 ;
+//        a +=1;
+//        a++;
+//        System.out.println(a);
+        //EKSİLTME
+//        int a = 5;
+//        a = a-1 ;
+//        a -=1;
+//        a--;
+//        System.out.println(a);
+//        Scanner scanner = new Scanner(System.in);
+//        int a,b;
+//        System.out.println("Sayılarınızı Giriniz;") ;
+//
+//        a= scanner.nextInt();
+//        b= scanner.nextInt();
+//        System.out.println("Toplamları: " + (a+b));
+//
+//    int age = 17;
+//    if (age < 18) {
+//        System.out.println("oy kullanamazsınız");
+//    }
+//    else {
+//        System.out.println("oy kullanabilirsiniz");
+//    }
+//
+//
+//
+        //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//        int op = 3;
+//        switch (op) {
+//            case 1:
+//                System.out.println("İşlem 1 ");
+//                break;
+//            case 2:
+//                System.out.println("İşlem 2 ");
+//                break;
+//            case 3:
+//                System.out.println("İşlem 3 ");
+//                break;
+//            default:
+//                System.out.println("Geçersiz İşlem.");
+//        int i = 0;
+//        while (i<10) {
+//            System.out.println("i'nin değeri:"+i );
+//            i+=2;
+//        }
+//        for (int i=0;i<10;i+=2){
+//               System.out.println("i'nin değeri "+i );
+//        }
+//    int i = 0;                                                  i değişkenini 0 olarak başlatır.
+//    while (true){                                                Sonsuz bir döngüye girer (while (true)).
+//           System.out.println("i'nin değeri "+ i);               Döngü içinde i değişkeninin değerini ekrana yazdırır.
+//           if (i ==5){                                           i 5'e eşit olduğunda, "Döngüden çıkılıyor" mesajını yazdırır ve döngüden çıkar.
+//               System.out.println("Döngüden çıkılıyor");         i 5'e eşit değilse, i'yi 1 artırır ve döngüye devam eder.
+//               break;
+////           }
+////           i++;
+        
+    }
+        }
+
+
