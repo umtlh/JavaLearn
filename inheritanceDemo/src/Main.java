30ca9202963190492f20e1ca6ee8a87f8b79b2a2
umtlh <umtlh@hotmail.com>
2024-06-26 15:12:43 +0300

    inheritanceDemo egitimi alındı.

---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        OgretmenKrediManager ogretmenkrediManager=new OgretmenKrediManager();
+        ogretmenkrediManager.Hesapla();
+    }
+}
