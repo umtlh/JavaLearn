a2f156a69ea7445a9a3080030db2daef724eb5be
umtlh <umtlh@hotmail.com>
2024-07-05 14:09:29 +0300

    exception mantıgı ve hiyerarsi anlasıldı. Demo yapildi.

---
+++ exceptionHandling/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try
+        {
+            int[] sayilar = new int []{1,2,3};
+            System.out.println(sayilar[5]);
+        }catch (Exception exception){
+            System.out.println(exception);
+        }finally {
+            System.out.println("Ben her türlü çalışırım");
+        }
+    }
+}
