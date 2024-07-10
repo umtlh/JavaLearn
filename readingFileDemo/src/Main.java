a731ed48d438b683fe2c69a6142edc99fde18613
umtlh <umtlh@hotmail.com>
2024-07-10 15:11:44 +0300

    Checked Exceptionlar 2 ile calismak.

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -6,10 +6,22 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
+
         BufferedReader reader = null;
+        int total=0;
         try {
             reader = new BufferedReader(new FileReader("D:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2024.1.3\\JavaProjects\\JavaLearn\\readingFileDemo\\src\\deneme.txt"));
-        }catch (FileNotFoundException e) {
+           String line =null;
+           while (true) {
+               try {
+                   if (!((line =reader.readLine())!=null)) break;
+               } catch (IOException e) {
+                   throw new RuntimeException(e);
+               }
+               total += Integer.valueOf(line);
+           }
+           System.out.println("Toplam = " + total);
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }finally {
             try
@@ -19,4 +31,4 @@ public class Main {
             }
         }
     }
-}
+        }
