7baa2404b1a40aafeaecd6317180f14e6af52635
umtlh <umtlh@hotmail.com>
2024-07-22 13:24:50 +0300

    dbhelper, select ifadelerinin çalıştırılması.

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,21 +1,25 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 
 
 public class Main {
 
-        static String userName="root";
-        static String password="12345";
-        static String dbUrl ="jdbc:mysql://localhost:3306/world";
-        public static void main(String[] args) throws SQLException{
+        public static <Country> void main(String[] args) throws SQLException{
             Connection connection =null;
+            DbHelper helper = new DbHelper();
+            Statement statement = null;
+            ResultSet resultSet;
             try{
-                connection = DriverManager.getConnection(dbUrl,userName,password);
-                System.out.println("Bağlantı oluştu");
+                connection = helper.getConnection();
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery("select country from sakila.country");
+
+                while (resultSet.next()){
+                    System.out.println(resultSet.getString("Country"));
+                }
+
             } catch (SQLException exception) {
-                System.out.println(exception.getMessage());
+                helper.showErrorMessage(exception);
             }
             finally {
                 connection.close();
